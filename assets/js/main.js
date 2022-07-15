5c6ed8aabc46219b7889a8b3d33cd85683761ff3
Pseudo <v.pseudo.11@gmail.com>
2022-07-14 21:32:49 -0500

    actualizado al completo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,5 @@
+import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle';
 import pintarProductos from './components/productos.js'
 import { pintarArticulos, agregarArticulo, removerArticulo, removerTodo, vaciarCarrito, comprar, articulos } from './components/carrito.js'
 
@@ -71,4 +73,23 @@ document.addEventListener('DOMContentLoaded', () => {
 
         pintarArticulos()
     })
+})
+
+const swiper = new Swiper(".slide-content", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    loopFillGroupWithBlank: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        dynamicBullets: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
 })
---
+++ index.js
@@ -0,0 +1,6 @@
+import './node_modules/swiper/swiper-bundle.esm.browser.min.js'
+import './assets/js/main.js'
+
+import './node_modules/swiper/swiper-bundle.css'
+import './assets/css/style.css'
+import './assets/css/utilities.css'
